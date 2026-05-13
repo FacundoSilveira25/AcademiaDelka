6764bee745268968f66e6f008f670624478653e6
Facundo Silveira <facundosilver2021@gmail.com>
2026-05-13 16:01:03 -0300

    fix: remover imports de React sin uso para Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Noticias } from './components/Noticias';
 import { QuienesSomos } from './components/QuienesSomos';
 import { Contacto } from './components/Contacto';

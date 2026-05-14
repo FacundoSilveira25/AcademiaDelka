a6238782adcc3306f08c447bd1f90dccbd197288
Facundo Silveira <facundosilver2021@gmail.com>
2026-05-14 11:30:40 -0300

    feat: agrego logo al header

--- src/data.ts
+++ src/data.ts
@@ -26,7 +26,7 @@ export const instructores = [
     bio: "Master Javier del Castillo – VII Dan Director de la Academia DEL'KA-SULSA, Con una vida dedicada al estudio, perfeccionamiento y enseñanza de las artes marciales, el Master Javier del Castillo es un pilar fundamental del Taekwon-Do en Uruguay. Con más de tres décadas de trayectoria ininterrumpida, lidera la Academia DEL'KA-SULSA, forjando con excelencia técnica y rigor a múltiples generaciones de atletas, cinturones negros y nuevos instructores en todo el país. Lejos de limitarse a la enseñanza formativa, es un artista marcial que lidera con el ejemplo y se mantiene como un competidor activo de primer nivel. Su espíritu indomable lo ha llevado a destacar en el circuito de Full Contact y a participar en eventos de élite como el Taekwon-Do Pro Fighter. Actualmente, consolida su gran nivel competitivo disputando el prestigioso título de la ISKA (International Sport Karate Association), demostrando la vigencia y contundencia de sus habilidades en el ring. Además de su maestría en el Taekwon-Do y el Full Contact, su profundo conocimiento biomecánico y estratégico de los deportes de combate lo ha posicionado como un verdadero referente en el ámbito del boxeo. Su enfoque pedagógico fusiona los principios tradicionales del arte marcial —cortesía, integridad, perseverancia, autocontrol y espíritu indomable— con la intensidad y efectividad del combate deportivo de alto rendimiento, brindando a sus alumnos una formación verdaderamente integral.",
     logros: ["Campeón Mundial 2023", "Entrenador de la Selección", "Multiple Campeon Nacional"],
     record: "",
-    contacto: "carlos@academia.com"
+    contacto: ""
   },
   {
     id: "inst-2",
@@ -36,13 +36,19 @@ export const instructores = [
     bio: "Con una sólida trayectoria en las artes marciales y los deportes de contacto, Facundo Silveira es un instructor dedicado a potenciar las habilidades físicas y mentales de sus alumnos. Cuenta con una certificación de alto grado en Taekwon-Do ITF y es especialista en la enseñanza de Kickboxing y Boxeo. Su enfoque de enseñanza combina la disciplina tradicional y la precisión técnica del Taekwon-Do con la efectividad y el acondicionamiento dinámico del Boxeo y el Kickboxing. Además de su labor como docente, Facundo es un competidor activo en la escena del Kickboxing en Uruguay. Esta experiencia de primera mano en el ring le permite transmitir conocimientos reales, probados en combate, y entender a la perfección los procesos físicos y psicológicos por los que pasa un atleta, desde la preparación y el corte de peso hasta la estrategia en la pelea. Ya sea que busques aprender a defenderte, mejorar tu estado físico o prepararte para competir, Facundo te guiará con un entrenamiento estructurado, exigente y adaptado a tus metas.",
     logros: ["Campeona Mundial 2023 lucha por equipo", "Campeon Nacional 2022", "Luchador de KickBoxing Semi-Profesional"],
     record: "1-3",
-    contacto: "ana@academia.com"
+    contacto: "facundosilveira18@gmail.com"
   }
 ];
 
 export const sedes = [
   {
     id: 1,
+    nombre: "DEL'KA Salinas",
+    direccion: "Calle C1, Salinas Norte",
+    mapaUrl: "https://www.google.com/maps/place/Academia+Del'ka/@-34.7643714,-55.8446335,18.5z/data=!4m6!3m5!1s0x959ff5003192d365:0xb5bb1675c86e3505!8m2!3d-34.763765!4d-55.84545!16s%2Fg%2F11yskb6jfk?authuser=0&entry=ttu&g_ep=EgoyMDI2MDUxMS4wIKXMDSoASAFQAw%3D%3D" // Coordenadas de ejemplo
+  },
+  {
+    id: 2,
     nombre: "DEL'KA Montevideo",
     direccion: "Gral. Flores 2610, Esquina Rivadabia Montevideo",
     mapaUrl: "https://www.google.com/maps/place/Gimnasio+Dragon/@-34.8797021,-56.1835722,17z/data=!3m1!4b1!4m6!3m5!1s0x959f806b3319e163:0xf4fab471a60fb94d!8m2!3d-34.8797065!4d-56.1809973!16s%2Fg%2F11dfq5nqg8?entry=ttu&g_ep=EgoyMDI2MDUxMS4wIKXMDSoASAFQAw%3D%3D" // Coordenadas de ejemplo
